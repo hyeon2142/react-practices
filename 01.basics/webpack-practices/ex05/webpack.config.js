608a802f85d1191e028176af9504faa4e3add1fa
hyeon <hyeon2142@naver.com>
2021-11-24 09:45:42 +0900

    update

--- 01.basics/webpack-practices/ex05/webpack.config.js
+++ 01.basics/webpack-practices/ex05/webpack.config.js
@@ -5,12 +5,17 @@ module.exports ={
     entry: path.resolve('src/index.js'),
     output: {
         path: path.resolve('public'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        assertsModuleFilename: 'assets/images[hash]'
     },
     module: {
         rules: [{
             test: /\.(sa|sc|c)ss$/i,
             use:['style-loader', 'css-loader', 'sass-loader']
+        },{
+            test: /\.(png|gif|jpg?g|svg|ico|tiff?|bmp)$/i, //i,
+            user: ['assets/resource']
+        
         }]
     },
     devServer: {
