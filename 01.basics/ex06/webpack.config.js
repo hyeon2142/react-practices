d8f861c5ad683113d74ccf80de44811fcf73b56d
hyeon <hyeon2142@naver.com>
2021-11-24 12:26:33 +0900

    2021-11-24 수업내용

--- 01.basics/ex06/webpack.config.js
+++ 01.basics/ex06/webpack.config.js
@@ -5,21 +5,29 @@ module.exports ={
     entry: path.resolve('src/index.js'),
     output: {
         path: path.resolve('public'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        assetModuleFilename: 'assets/images/[hash][ext]'
     },
     module: {
         rules: [{
+            test: /\.js$/i,
+            exclude: /node_modules/,
+            use:['babel-loader']
+        }, {
             test: /\.(sa|sc|c)ss$/i,
             use:['style-loader', 'css-loader', 'sass-loader']
+        }, {
+            test: /\.(png|gif|jpe?g|svg|ico|tiff?|bmp)$/i,
+            type: 'asset/resource'
         }]
     },
+    devtool: "eval-source-map",
     devServer: {
-        // contentBase: path.resolve('public'),
         host: '0.0.0.0',
         port: 9999,
         // inline: true
-        liveReload: true,
         hot: false,
-        compress: true
+        compress: true,
+        historyApiFallback: true
     }
 }
