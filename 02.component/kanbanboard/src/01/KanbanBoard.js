30a740f101da7993f0daad131737863dc6f13dfe
hyeon <hyeon2142@naver.com>
2021-11-26 12:25:03 +0900

    2021-11-26

---
+++ 02.component/kanbanboard/src/01/KanbanBoard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cards from './data.json';
+import './assets/styles.css';
+
+export default function() {
+    return (
+        <div className='KanbanBoard'>
+
+        
+        </div>
+    )
+}
